a8095a5828419ea6b18871daa0e8e80ab4f4c1e3
KayVeeZ <kshitijvashisth@gmail.com>
2025-08-05 19:33:23 +0100

    aesthetic changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,15 @@
 // components/Sidebar.jsx
 import { useState } from 'react'
 import { IonIcon } from '@ionic/react'
-import { chevronDown, mailOutline, logoLinkedin, logoGithub } from 'ionicons/icons'
+import { mailOutline, logoLinkedin } from 'ionicons/icons'
 import ProfileCard from './ProfileCard/ProfileCard'
 
+import TextType from './TextType/TextType'
+
 import { FaGoogleScholar } from "react-icons/fa6";
+import { SiScopus } from "react-icons/si";
+import { FaOrcid } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
 
 export default function Sidebar() {
   const [showContacts, setShowContacts] = useState(false)
@@ -27,50 +32,61 @@ export default function Sidebar() {
             showUserInfo={true}
             enableTilt={true}
             enableMobileTilt={false}
-            onContactClick={() => console.log('Contact clicked')}
+            onContactClick={() => 
+              window.location.href = "mailto:jksinha@gloneuro.org?subject=Hello&body=I%20wanted%20to%20contact%20you regarding..."
+            }
           />
-          <h1 className="name">Dr. Jitendra K. Sinha</h1>
-          <p className="title">Neuroscientist</p>
+
+          <br />
+          <TextType
+            text={["Hello!👋", "How are you doing?", "Welcome to my Profile!"]}
+            typingSpeed={75}
+            pauseDuration={1500}
+            showCursor={true}
+            cursorCharacter="█"
+          />
+          <br />
+          <br />
         </div>
 
-        <button className="info_more-btn" onClick={() => setShowContacts(!showContacts)}>
-          <span>Show Contacts</span>
-          <IonIcon icon={chevronDown} />
-        </button>
+
       </div>
 
-      {showContacts && (
-        <div className="sidebar-info_more">
-          <div className="separator"></div>
 
-          <ul className="contacts-list">
-            <li className="contact-item">
-              <div className="icon-box">
-                <IonIcon icon={mailOutline} />
-              </div>
-              <div className="contact-info">
-                <p className="contact-title">Email</p>
-                <a href="mailto:your@email.com" className="contact-link">your@email.com</a>
-              </div>
-            </li>
-          </ul>
+      <div className="sidebar-info_more">
 
-          <div className="separator"></div>
 
-          <ul className="social-list">
-            <li className="social-item">
-              <a href="https://www.linkedin.com/in/drjitendrakumarsinha/" className="social-link" target='_blank'>
-                <IonIcon icon={logoLinkedin} />
-              </a>
-            </li>
-            <li className="social-item">
-              <a href="https://scholar.google.com/citations?user=9OkrGBwAAAAJ&hl=en&oi=ao" className="social-link" target="_blank">
-                <FaGoogleScholar />
-              </a>
-            </li>
-          </ul>
-        </div>
-      )}
+
+
+        <ul className="social-list">
+          <li className="social-item">
+            <a href="mailto:jksinha.hi@gmail.com" className="social-link">
+              <BiLogoGmail />
+            </a>
+          </li>
+          <li className="social-item">
+            <a href="https://www.linkedin.com/in/drjitendrakumarsinha/" className="social-link" target='_blank'>
+              <IonIcon icon={logoLinkedin} />
+            </a>
+          </li>
+          <li className="social-item">
+            <a href="https://scholar.google.com/citations?user=9OkrGBwAAAAJ&hl=en&oi=ao" className="social-link" target="_blank">
+              <FaGoogleScholar />
+            </a>
+          </li>
+          <li className="social-item">
+            <a href="https://www.scopus.com/authid/detail.uri?authorId=35800399200" className="social-link" target="_blank">
+              <SiScopus />
+            </a>
+          </li>
+          <li className="social-item">
+            <a href="https://orcid.org/0000-0002-7444-6932" className="social-link" target="_blank">
+              <FaOrcid />
+            </a>
+          </li>
+        </ul>
+      </div>
+
     </aside>
   )
 }
