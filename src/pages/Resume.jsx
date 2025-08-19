61935ba684131d116a62a684fd1caecdd7612a5d
KayVeeZ <kshitijvashisth@gmail.com>
2025-08-19 14:17:14 +0100

    [feature] disabled download cv button

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,8 +4,8 @@ import { bookOutline } from 'ionicons/icons'
 
 export default function Resume() {
   return (
-    <article className="resume" data-page="resume" style={{ display: 'block'}}>
-      <br/><br/>
+    <article className="resume" data-page="resume" style={{ display: 'block' }}>
+      <br /><br />
       <header>
         <h2 className="h2 article-title">Resume</h2>
       </header>
@@ -71,51 +71,55 @@ export default function Resume() {
 
           <li className="timeline-item">
             <h4 className="h4 timeline-item-title"> Assistant Professor (Grade III)</h4>
-            <p className="timeline-text">Amity Institute of Neuropsychology and 
-Neurosciences (AINN), Amity University UP, Noida, India</p>
+            <p className="timeline-text">Amity Institute of Neuropsychology and
+              Neurosciences (AINN), Amity University UP, Noida, India</p>
             <span>September 2017 – March 2022r</span>
           </li>
 
           <li className="timeline-item">
             <h4 className="h4 timeline-item-title">SERB – National Post-Doctoral Fellow and Research Associate</h4>
-            <p className="timeline-text">CSIR - 
-Centre for Cellular and Molecular Biology (CCMB), Hyderabad, India</p>
+            <p className="timeline-text">CSIR -
+              Centre for Cellular and Molecular Biology (CCMB), Hyderabad, India</p>
             <span>January 2016 – August 2017</span>
           </li>
 
           <li className="timeline-item">
             <h4 className="h4 timeline-item-title">ICMR/ NIN-Senior Research Fellow and Research Associate</h4>
-            <p className="timeline-text"> National 
-Institute of Nutrition (NIN), Hyderabad, India</p>
+            <p className="timeline-text"> National
+              Institute of Nutrition (NIN), Hyderabad, India</p>
             <span>July 2009 – December 2015</span>
             <ul className="timeline-text">
-              <li>6 months as <strong className='force-bold'>IBRO APRC Research Exchange Fellow / UNSW Visiting Fellow</strong> at University of 
-New South Wales, Sydney, Australia</li><br/>
-              <li> 4 months as <strong className='force-bold'>Young Investigator Fellow & Ricerca Assegnista 
-(Research Fellow)</strong> at Università degli Studi di Verona, Verona, Italy</li>
+              <li>6 months as <strong className='force-bold'>IBRO APRC Research Exchange Fellow / UNSW Visiting Fellow</strong> at University of
+                New South Wales, Sydney, Australia</li><br />
+              <li> 4 months as <strong className='force-bold'>Young Investigator Fellow & Ricerca Assegnista
+                (Research Fellow)</strong> at Università degli Studi di Verona, Verona, Italy</li>
             </ul>
           </li>
 
           <li className="timeline-item">
             <h4 className="h4 timeline-item-title">Junior Research Fellow (CSIR)</h4>
-            <p className="timeline-text">Dept of Neurophysiology, National 
-Institute of Mental Health And Neuro Sciences, Bangalore, India</p>
+            <p className="timeline-text">Dept of Neurophysiology, National
+              Institute of Mental Health And Neuro Sciences, Bangalore, India</p>
             <span>September 2007 – February 2009</span>
           </li>
 
           <li className="timeline-item">
             <h4 className="h4 timeline-item-title">BCIL Research Assistant & Trainee</h4>
-            <p className="timeline-text">Biotech Industrial Training Program of 
-DBT, Government of India & Biotech Consortium India Ltd. (BCIL)</p>
+            <p className="timeline-text">Biotech Industrial Training Program of
+              DBT, Government of India & Biotech Consortium India Ltd. (BCIL)</p>
             <span>June 2006 – July 2007</span>
           </li>
         </ol>
       </section>
 
-      <button className="form-btn" type="button">
+      {/* <button className="form-btn button-disabled" type="button">
         <IonIcon icon={documentTextOutline} />
         <a href='/CV/CV_Dr Jitendra.pdf' target='_blank'>
         <span>Download CV</span></a>
+      </button> */}
+      <button className="form-btn" type="button" disabled>
+        <IonIcon icon={documentTextOutline} />
+        <span className="button-disabled">Download CV</span>
       </button>
     </article>
   )
